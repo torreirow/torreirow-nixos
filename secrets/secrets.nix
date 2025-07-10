38c0de5468246a09ed57fb6c91bdc5f738ede69e
Wouter van der Toorren <torreirow@toorren.net>
2025-07-10 13:04:13 +0200

    rekey with new host key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,25 +4,26 @@ let
   users = [ wtoorren workload ];
 
   wtoorren_workstation = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGt4v9ns8+HmnoyTMGvH7bNpgN4MkTgYst2YgSYzPTfO";
+  malandro_workstation = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFwxP+MM4rwc6T5g4NZrjSYxQ3yRwlFCK6pnXZt/JWCX";
   #prod = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB65TTLa+J49yEthFh98kadBZTIMFQSLO4uw7xgfjGy/";
   prod = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICKG+/j0X7t8IhYpCeLExMV3ddsgC4B7zh5tybEJpnLE";
   nonprod = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMRZYXbx6yNqp7wCxztD2pHILbe+Kou+s15GnNaq0+b1";
 
-  systems = [ nonprod prod wtoorren_workstation ];
+  systems = [ nonprod prod wtoorren_workstation malandro_workstation];
   
 in
 
 {
-  "secret1.age".publicKeys = users ++ [ wtoorren_workstation ];
-  "secret2.age".publicKeys = users ++ [ wtoorren_workstation ];
-  "togglwtoorren.age".publicKeys = users ++ [ wtoorren_workstation ];
-  "loboskey.age".publicKeys = users ++ [ wtoorren_workstation ];
-  "spotifywto.age".publicKeys = users ++ [ wtoorren_workstation ];
-  "update_laptop.age".publicKeys = users ++ [ wtoorren_workstation ];
-  "atticwto.age".publicKeys = users ++ [ wtoorren_workstation ];
-  "kar01-lobos-ovpn.age".publicKeys = users ++ [ wtoorren_workstation ];
-  "improvement-ovpn.age".publicKeys = users ++ [ wtoorren_workstation ];
-  "aider.age".publicKeys = users ++ [ wtoorren_workstation ];
+  "secret1.age".publicKeys = users ++ [ wtoorren_workstation malandro_workstation ];
+  "secret2.age".publicKeys = users ++ [ wtoorren_workstation malandro_workstation ];
+  "togglwtoorren.age".publicKeys = users ++ [ wtoorren_workstation malandro_workstation ];
+  "loboskey.age".publicKeys = users ++ [ wtoorren_workstation malandro_workstation ];
+  "spotifywto.age".publicKeys = users ++ [ wtoorren_workstation malandro_workstation ];
+  "update_laptop.age".publicKeys = users ++ [ wtoorren_workstation malandro_workstation ];
+  "atticwto.age".publicKeys = users ++ [ wtoorren_workstation malandro_workstation ];
+  "kar01-lobos-ovpn.age".publicKeys = users ++ [ wtoorren_workstation malandro_workstation ];
+  "improvement-ovpn.age".publicKeys = users ++ [ wtoorren_workstation malandro_workstation ];
+  "aider.age".publicKeys = users ++ [ wtoorren_workstation malandro_workstation ];
   
   # Jitsi Meet passwords
   "jitsi-focus-password.age".publicKeys = users ++ systems;
