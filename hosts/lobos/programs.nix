6680097f674d60c09b84e9092901e36e7f5a29b0
TorreiroW <githubwto@toorren.net>
2025-10-29 12:09:45 +0100

    update

--- hosts/lobos/programs.nix
+++ hosts/lobos/programs.nix
@@ -3,9 +3,21 @@
 {
   
   environment.systemPackages = with pkgs; [
+    desktop-file-utils
+    neofetch
+    dstp
+    bambu-studio
+    android-tools   # voor adb
+    perl
+    simple-mtpfs    # om telefoon te mounten
+    wineWowPackages.full winetricks
     #bluez
     #cooklang
     #flameshot
+    openai
+    unstable.lynis
+    fastfetch
+    go-mtpfs
     direnv
     agenix
     alacritty
@@ -116,7 +128,7 @@
     terraform
     terraform-docs
     tfswitch
-    thunderbird
+    thunderbird-140
     tmuxPlugins.catppuccin
     translate-shell
 #    unstable.aider-chat-full
@@ -148,6 +160,62 @@
 xdg-desktop-portal
   ] ;
 
+programs.nix-ld = {
+    enable = true;
+
+    libraries = with pkgs; [
+      # C/C++ runtime
+      stdenv.cc.cc
+
+      # Core GLib/GObject/GIO
+      glib
+      dbus
+
+      # GTK stack
+      gtk3
+      atk
+      at-spi2-core
+      at-spi2-atk
+      gdk-pixbuf
+      pango
+      cairo
+
+      # Audio
+      alsa-lib
+
+      # Printing (needed by Electron)
+      cups
+
+      # Crypto/Networking
+      nss
+      nspr
+      nssTools
+
+      # Fonts
+      fontconfig
+      freetype
+      expat
+
+      # Graphics & GPU
+      mesa
+      libdrm
+      libglvnd
+      libgbm
+
+    ] ++ (with pkgs.xorg; [
+      libX11
+      libXcomposite
+      libXdamage
+      libXext
+      libXfixes
+      libXrandr
+      libXcursor
+      libXi
+      libXrender
+      libxcb
+      libxkbcommon
+    ]);
+  };
 
 #fonts.packages = with pkgs; [
 #  open-sans
