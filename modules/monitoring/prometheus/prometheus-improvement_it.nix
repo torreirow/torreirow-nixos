9cc184c70dba3f20d350416ba46a8fc7a67c2365
TorreiroW <githubwto@toorren.net>
2025-11-02 16:25:18 +0100

    fix scrape config setup

--- modules/monitoring/prometheus/prometheus-improvement_it.nix
+++ modules/monitoring/prometheus/prometheus-improvement_it.nix
@@ -1,31 +1,29 @@
-{ config, pkgs, ... }:
+{ lib, ... }:
 
 {
-  services.prometheus = {
-    scrapeConfigs = [
-      {
-        job_name = "blackbox-improvement_it";
-        metrics_path = "/probe";
-        params.module = [ "http_2xx" ];
-        file_sd_configs = [
-          {
-            files = [ "/etc/prometheus/improvement_it-urls.yaml" ];
-            refresh_interval = "5m";
-          }
-        ];
-        relabel_configs = [
-          { source_labels = [ "__address__" ]; target_label = "__param_target"; }
-          { source_labels = [ "__param_target" ]; target_label = "instance"; }
-          { target_label = "__address__"; replacement = "localhost:9115"; }
-        ];
-      }
-    ];
+  services.prometheus.scrapeConfigs = lib.mkAfter [
+    {
+      job_name = "blackbox-improvement_it";
+      metrics_path = "/probe";
+      params.module = [ "http_2xx" ];
 
-    ruleFiles = [ ./alerts/improvement_it.yml ];
-  };
+      file_sd_configs = [
+        {
+          files = [ "/etc/prometheus/customers/improvement_it/probes/urls.yaml" ];
+          refresh_interval = "5m";
+        }
+      ];
 
-  environment.etc."prometheus/improvement_it-urls.yaml" = {
-    source = ./probes/improvement_it/urls.yaml;
+      relabel_configs = [
+        { source_labels = [ "__address__" ]; target_label = "__param_target"; }
+        { source_labels = [ "__param_target" ]; target_label = "instance"; }
+        { target_label = "__address__"; replacement = "localhost:9115"; }
+      ];
+    }
+  ];
+
+  environment.etc."prometheus/customers/improvement_it/probes/urls.yaml" = {
+    source = ./customers/improvement_it/probes/urls.yaml;
     mode = "0644";
     user = "prometheus";
     group = "prometheus";
