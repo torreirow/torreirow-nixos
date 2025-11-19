992f39a5b69c1cd793f6473f66699616f9362f12
TorreiroW <githubwto@toorren.net>
2025-11-19 12:36:33 +0100

    update

--- modules/monitoring/default.nix
+++ modules/monitoring/default.nix
@@ -4,4 +4,7 @@
     ./prometheus
     ./grafana
   ];
+    services.vulnix-exporter.enable = true;
+    services.vulnix-exporter.port = 9109;
+    services.vulnix-exporter.interval = "monthly";  # of "1M"
 }
