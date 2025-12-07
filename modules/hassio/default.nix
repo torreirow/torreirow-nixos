3628f32fa04222636a113f603193254de8712966
Wouter van der Toorren <torreirow@toorren.net>
2025-12-07 19:32:46 +0100

    add hassion.nix and rename it to default

---
+++ modules/hassio/default.nix
@@ -0,0 +1,19 @@
+{
+  services.home-assistant = {
+    enable = true;
+    extraComponents = [
+      # Components required to complete the onboarding
+      "analytics"
+      "google_translate"
+      "met"
+      "radio_browser"
+      "shopping_list"
+      # Recommended for fast zlib compression
+      # https://www.home-assistant.io/integrations/isal
+      "isal"
+    ];
+    config = null;
+    lovelaceConfig = null;
+    configDir="/etc/home-assistant";
+  };
+}
