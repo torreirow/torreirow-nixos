ab28bd990ab5cde576f09071f28243221a0986f4
TorreiroW <githubwto@toorren.net>
2025-12-11 22:43:03 +0100

    lobos major push

--- configuration.nix
+++ configuration.nix
@@ -70,13 +70,18 @@
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
+    #pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;A
+    extraPackages = with pkgs; [
+      alsa-card-profiles
+      pipewire
+      pipewire-alsa
+      pipewire-pulse
+      pipewire-jack
+    ];
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
