8fbb8bba2785cc66129eba3ad1a1700175f6a309
Wouter van der Toorren <torreirow@toorren.net>
2025-12-26 21:35:06 +0100

    update

--- modules/wg.nix
+++ modules/wg.nix
@@ -1,56 +1,63 @@
 { config, pkgs, ... }:
+
 {
   virtualisation.docker.enable = true;
 
-  networking.firewall.allowedUDPPorts = [ 51820 ];
-  networking.firewall.allowedTCPPorts = [ 51821 ]; # wg-easy web UI
+  # Firewall
+  networking.firewall = {
+    enable = true;
+    allowedUDPPorts = [ 51820 ];
+    allowedTCPPorts = [ 51821 ];
+    checkReversePath = "loose";
+    trustedInterfaces = [ "docker0" ];
+  };
 
-  # NAT zodat client's internet via server gaat
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+
+  # NAT voor Docker bridge
   networking.nat = {
     enable = true;
-    externalInterface = "eth0";   # aanpassen aan jouw interface
-    internalInterfaces = [ "wg0" ];
+    externalInterface = "enp3s0";
+    internalInterfaces = [ "docker0" ];
   };
 
-  # wg-easy container
-  virtualisation.oci-containers.containers."wg-easy" = {
-    image = "ghcr.io/wg-easy/wg-easy:latest";
-    autoStart = true;
-
-    environment = {
-      WG_HOST = "wg.toorren.net";
-      PASSWORD_HASH = "$argon2i$v=19$m=4096,t=3,p=1$c2FsdEl0V2l0aFNhbHQ$iXQSPtFr16sdDm39PKQiGU2s6OtCjP2ZocsF1XIOlUs"; 
-      WG_PORT = "51820";
-      WG_DEFAULT_DNS = "1.1.1.1";
-      WG_DEFAULT_ADDRESS = "10.8.0.x";
-      WG_ALLOWED_IPS = "0.0.0.0/0,::/0"; # full tunnel
+  # wg-easy DOCKER container (GEEN host networking)
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers.wg-easy = {
+      image = "ghcr.io/wg-easy/wg-easy:latest";
+      ports = [
+        "51820:51820/udp"
+        "51821:51821/tcp"
+      ];
+      volumes = [ "/var/lib/wg-easy:/etc/wireguard" ];
+      environment = {
+        WG_HOST = "wg.toorren.net";
+        PASSWORD_HASH = "$2a$12$2kO66Q7Xg4JI/n2QzXW9ROTZ0O2yJA/NJCuYMDl.YU9g8PS.ZYJsi";
+        WG_DEFAULT_DNS = "1.1.1.1";
+        WG_ALLOWED_IPS = "0.0.0.0/0";  # Belangrijk voor Android full tunnel
+      };
+      capabilities = {
+        NET_ADMIN = true;
+        SYS_MODULE = true;
+      };
+      autoStart = true;
     };
-
-    # Volumes voor persistent config
-    volumes = [
-      "/var/lib/wg-easy:/etc/wireguard"
-    ];
-
-    ports = [
-      "51820:51820/udp"
-      "51821:51821/tcp"
-    ];
-
-    extraOptions = [
-      "--cap-add=NET_ADMIN"
-      "--cap-add=SYS_MODULE"
-      "--sysctl=net.ipv4.ip_forward=1"
-      "--sysctl=net.ipv4.conf.all.src_valid_mark=1"
-    ];
   };
 
+  services.nginx.enable = true;
   services.nginx.virtualHosts."wg.toorren.net" = {
     enableACME = true;
     forceSSL = true;
-    locations = {
-      "/" = {
-        proxyPass = "http://192.168.2.52:51821";
-      };
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:51821";
+      proxyWebsockets = true;
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+      '';
     };
   };
 }
