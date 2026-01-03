7cb7519dfaf45d1ec17f367caa135af068ec8005
TorreiroW <githubwto@toorren.net>
2026-01-03 11:55:20 +0100

    fix tmux

--- home/tmux.nix
+++ home/tmux.nix
@@ -1,49 +1,44 @@
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
   programs.tmux = {
     enable = true;
     clock24 = true;
-    plugins = [
-      pkgs.tmuxPlugins.gruvbox
-      pkgs.tmuxPlugins.sensible
-      pkgs.tmuxPlugins.yank
+
+    # BELANGRIJK voor Alacritty
+    terminal = "xterm-256color";
+
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      yank
+      resurrect
+      gruvbox
     ];
+
     extraConfig = ''
-    # Plugins
-    set -g @plugin 'tmux-plugins/tpm'
-    set -g @plugin 'tmux-plugins/tmux-sensible'
-    set -g @plugin 'tmux-plugins/tmux-yank'
-    set -g @plugin 'tmux-plugins/tmux-resurrect'
-    set -g @plugin 'egel/tmux-gruvbox'
+      ##### Basisinstellingen #####
+      set -g prefix C-a
+      unbind C-b
+      unbind r
+      bind r source-file ~/.config/tmux/tmux.conf \; display-message "Reloaded!"
+      set -g mouse on
+      set -g base-index 1
+      set -g renumber-windows on
 
-    # Basisinstellingen
-    set -g prefix C-a
-    unbind r
-    bind r source-file ~/.config/tmux/tmux.conf \; display-message "Reloaded!"
-    set -g mouse on
-    set -g base-index 1
-    set -g renumber-windows on
-    bind-key g set-window-option synchronize-panes \; display-message "synchronize-panes is now #{?pane_synchronized,on,off}"
+      bind-key g set-window-option synchronize-panes \;\
+        display-message "synchronize-panes is now #{?pane_synchronized,on,off}"
 
-    # Gruvbox settings
-    set -g @tmux-gruvbox 'dark'
-    set -g @tmux-gruvbox-statusbar-alpha 'true'
+      ##### Gruvbox #####
+      set -g @tmux-gruvbox 'dark'
+      set -g @tmux-gruvbox-statusbar-alpha 'true'
 
-    # Prefix Highlight stijl (optioneel)
-    set -g @prefix_highlight_fg 'black'
-    set -g @prefix_highlight_bg 'yellow'
+      ##### Clipboard (Alacritty / Wayland / X11) #####
+      set -as terminal-features ',xterm-256color:clipboard'
 
-    set -as terminal-features ',rxvt-unicode-256color:clipboard'
-
-    # Zet statusbar expliciet NA het thema
-    set -g status-left '#{prefix_highlight} | #S '
-
-    # TPM loader – moet altijd onderaan staan!
-      run '~/.config/tmux/plugins/tpm/tpm'
-      set -g status-right '#[bg=default,fg=#504945,nobold,nounderscore,noitalics]#[bg=#504945,fg=#a89984] %Y-%m-%d  %H:%M #[bg=#504945,fg=#bdae93,nobold,noitalics,nounderscore]#[bg=#bdae93,fg=#3c3836] #h #[bg=#504945,fg=#a89984]  ⌨️ #(tmux show-option -gqv prefix) #('
-
-    ''
-    ;
+      ##### Statusbar #####
+      set -g status-left "#S "
+      set -g status-right "#[fg=#a89984]%Y-%m-%d  %H:%M #[fg=#bdae93] #h #[fg=#a89984] ⌨ #{prefix}"
+    '';
   };
 }
+
