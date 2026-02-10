fe3615a69ca29f24bbf3bc47150994ca9302cff3
Wouter van der Toorren <torreirow@toorren.net>
2026-02-10 12:32:44 +0100

    linnyweb setup

--- modules/gitea.nix
+++ modules/gitea.nix
@@ -2,38 +2,30 @@
 
 {
 
-  age.secrets.postfix-sasl-password = {
-    file = ./secrets/gitea-admin.age;
+  age.secrets.gitea-admin = {
+    file = ../secrets/gitea-admin.age;
   };
 
-  servive.gitea = {
+  services.gitea = {
     enable = true;
     stateDir = "/var/lib/gitea";
-    siteKey = "toorrenaer";
-    captcha.enable = false;
-    settings = {
-      "cron.sync_external_users" = {
-        RUN_AT_START = true;
-        SCHEDULE = "@every 24h";
-        UPDATE_EXISTING = true;
-      };
-      #mailer = {
-      #  ENABLED = true;
+
       #  PROTOCOL = "smtp+starttls";
       #  SMTP_ADDR = "smtp.example.org";
       #  SMTP_PORT = "587";
       #  FROM = "Gitea Service <do-not-reply@example.org>";
       #  USER = "do-not-reply@example.org";
       #};
-      other = {
-        SHOW_FOOTER_VERSION = false;
-      };
-    };
     settings.server.DOMAIN = "git.toorren.net";
     database.user = "gitea";
     database.type = "postgres";
-    database.paassword = config.age.secrets.gitea-admin.path;
+    #database.password = config.age.secrets.gitea-admin.path;
+    database.passwordFile = config.age.secrets.gitea-admin.path;
 
+    captcha = {
+      siteKey = "toorrenaer";
+      enable = false;
+    };
   };
 }
 
