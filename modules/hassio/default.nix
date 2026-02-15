7479b99a817e3ba068f3d1e107ab89f71bed8cab
Wouter van der Toorren <torreirow@toorren.net>
2026-02-15 19:29:26 +0100

    setup mqtt

--- modules/hassio/default.nix
+++ modules/hassio/default.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 
 {
+  # Create stable /dev/zigbee symlink for Sonoff Zigbee USB dongle
+  services.udev.extraRules = ''
+    SUBSYSTEM=="tty", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", ATTRS{serial}=="0001", SYMLINK+="zigbee"
+  '';
+
   virtualisation.oci-containers = {
     backend = "docker";
 
@@ -12,15 +17,64 @@
       ];
       extraOptions = [
         "--network=host"
-        "--privileged"
-        "--volume=/dev/serial/by-id:/dev/serial/by-id:rw"
         "--volume=/run/dbus:/run/dbus:ro"
+      ];
+    };
+
+    containers.zigbee2mqtt = {
+      image = "koenkk/zigbee2mqtt:latest";
+      environment = {
+        TZ = "Europe/Amsterdam";
+      };
+      volumes = [
+        "/var/lib/zigbee2mqtt:/app/data"
+        "/run/udev:/run/udev:ro"
+      ];
+      extraOptions = [
+        "--network=host"
+        "--device=/dev/zigbee:/dev/zigbee"
+        "--group-add=27"
       ];
     };
   };
 
+  # Create zigbee2mqtt directories
+  systemd.tmpfiles.rules = [
+    "d /var/lib/zigbee2mqtt 0755 root root -"
+  ];
+
+  # Create zigbee2mqtt config file
+  environment.etc."zigbee2mqtt/configuration.yaml" = {
+    text = ''
+      homeassistant: true
+      permit_join: false
+      mqtt:
+        base_topic: zigbee2mqtt
+        server: mqtt://127.0.0.1:1883
+      serial:
+        port: /dev/zigbee
+        adapter: zstack
+      frontend:
+        enabled: true
+        port: 8086
+      advanced:
+        log_level: info
+        network_key: GENERATE
+        pan_id: GENERATE
+        ext_pan_id: GENERATE
+    '';
+  };
+
+  # Copy config to the right location
+  system.activationScripts.zigbee2mqttConfig = ''
+    mkdir -p /var/lib/zigbee2mqtt
+    if [ ! -f /var/lib/zigbee2mqtt/configuration.yaml ]; then
+      cp /etc/zigbee2mqtt/configuration.yaml /var/lib/zigbee2mqtt/configuration.yaml
+    fi
+  '';
+
   networking.firewall = {
-    allowedTCPPorts = [ 8123 ];
+    allowedTCPPorts = [ 8123 8086 ];
   };
 
   services.nginx.virtualHosts."homeassistant.toorren.net" = {
@@ -34,5 +88,40 @@
     };
   };
 
+  services.nginx.virtualHosts."zigbee2mqtt.toorren.net" = {
+    useACMEHost = "toorren.net";
+    forceSSL = true;
+    locations = {
+      "/" = {
+        proxyPass = "http://127.0.0.1:8086";
+        proxyWebsockets = true;
+        extraConfig = ''
+          auth_request /authelia;
+          error_page 401 = @authelia_portal;
 
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+
+      "@authelia_portal" = {
+        extraConfig = ''
+          return 302 https://auth.toorren.net/?rd=$scheme://$http_host$request_uri;
+        '';
+      };
+
+      "/authelia" = {
+        proxyPass = "http://127.0.0.1:9091/api/verify";
+        extraConfig = ''
+          internal;
+          proxy_set_header X-Original-URL $scheme://$http_host$request_uri;
+          proxy_set_header X-Forwarded-For $remote_addr;
+          proxy_set_header Content-Length "";
+          proxy_pass_request_body off;
+        '';
+      };
+    };
+  };
 }
