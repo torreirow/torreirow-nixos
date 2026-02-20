b4560d0b6abf815817facf683bd424b9519fe6b1
Wouter van der Toorren <githubwto@toorren.net>
2026-02-20 14:14:53 +0100

    malandro3 (#26)
    * remove stirling
    
    * update docu
    
    ---------
    
    Co-authored-by: Wouter van der Toorren <torreirow@toorren.net>

--- modules/nginxendpoints.nix
+++ modules/nginxendpoints.nix
@@ -2,21 +2,4 @@
 
 {
 
-services.nginx = {
-  enable = true;
-
-  virtualHosts."status.toorren.net" = {
-    locations."/endpoints" = {
-      extraConfig = ''
-        gzip off;
-        include ${pkgs.nginx}/conf/fastcgi_params;
-        fastcgi_pass unix:${config.services.fcgiwrap.socketAddress};
-        fastcgi_param SCRIPT_FILENAME /usr/local/bin/list-endpoints.sh;
-      '';
-    };
-  };
-};
-
-services.fcgiwrap.enable = true;
-
 }
