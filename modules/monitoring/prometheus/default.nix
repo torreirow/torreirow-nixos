1f4a0d7c24907913268d0a9dea2ba2ef13145753
Wouter van der Toorren <torreirow@toorren.net>
2026-03-21 11:29:44 +0100

    torreiro url monitoring

--- modules/monitoring/prometheus/default.nix
+++ modules/monitoring/prometheus/default.nix
@@ -3,6 +3,7 @@
   imports = [
     ./prometheus.nix
     ./prometheus-technative.nix
+    ./prometheus-torreiro.nix
     # ./alertmanager-base.nix  # Disabled - telegram config gebruikt
     ./alertmanager-telegram.nix  # Telegram notificaties
     # ./alertmanager-slack.nix  # Uncomment om Slack notificaties in te schakelen
--- modules/monitoring/prometheus/exporters/blackbox.nix
+++ modules/monitoring/prometheus/exporters/blackbox.nix
@@ -5,6 +5,7 @@
     port = 9115;
     configFile = pkgs.writeText "blackbox.yml" ''
       modules:
+        # Externe HTTPS checks (SSL certificaat + bereikbaarheid)
         http_2xx:
           prober: http
           timeout: 15s
@@ -15,6 +16,22 @@
             no_follow_redirects: false
             preferred_ip_protocol: "ip4"
             valid_http_versions:
+              - "HTTP/1.1"
+              - "HTTP/2.0"
+
+        # Interne HTTP checks (localhost endpoints, geen SSL vereist)
+        http_2xx_internal:
+          prober: http
+          timeout: 10s
+          http:
+            fail_if_not_ssl: false
+            ip_protocol_fallback: false
+            method: GET
+            no_follow_redirects: false
+            preferred_ip_protocol: "ip4"
+            valid_status_codes: [200, 302]
+            valid_http_versions:
+              - "HTTP/1.0"
               - "HTTP/1.1"
               - "HTTP/2.0"
     '';
