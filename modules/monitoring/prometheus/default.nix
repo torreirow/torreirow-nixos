52759fdf67dab8f44983aa00c599a5a5305b74a5
Wouter van der Toorren <torreirow@toorren.net>
2026-03-21 01:14:49 +0100

    prometheus on telegram and new (test) alerts

--- modules/monitoring/prometheus/default.nix
+++ modules/monitoring/prometheus/default.nix
@@ -2,9 +2,9 @@
 {
   imports = [
     ./prometheus.nix
-    ./prometheus-improvement_it.nix
     ./prometheus-technative.nix
-    ./alertmanager-base.nix
+    # ./alertmanager-base.nix  # Disabled - telegram config gebruikt
+    ./alertmanager-telegram.nix  # Telegram notificaties
     # ./alertmanager-slack.nix  # Uncomment om Slack notificaties in te schakelen
     ./exporters/blackbox.nix
     ./exporters/vulnix.nix
