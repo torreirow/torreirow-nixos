1f76c57f86afe7e4d04cb306a686930db33d8521
TorreiroW <githubwto@toorren.net>
2026-04-11 10:12:54 +0200

    update

--- home/dotfiles/strawberry-radio-streams.nix
+++ home/dotfiles/strawberry-radio-streams.nix
@@ -4,27 +4,28 @@
 
 [
   {
-    name = "SevenFM";
-    url = "https://25583.live.streamtheworld.com/SEVENFMAAC.aac";
-  }
-  {
     name = "Bright FM";
-    url = "http://brightfm.streamserver.net:8000";
+    url = "https://brightfmaacplus.streamserver.net/stream";
+    thumbnail = "https://nl.radio.net/175/brightfm.png?version=766765906c9f0db35e672ab3570192aa21f5f681";
   }
   {
-    name = "Bright FM Plus";
-    url = "https://brightplus.hdsserver.net/stream";
+    name = "SevenFM";
+    url = "http://25583.live.streamtheworld.com/SEVENFMAAC.aac";
+    thumbnail = "https://www.sevenfm.nl/wp-content/uploads/2021/03/cropped-Logo-SevenFM-512x512-1-192x192.png";
   }
   {
     name = "Christian Regular";
     url = "http://stream-14.aiir.com/o8yaycnysb6tv";
-  }
-  {
-    name = "RFM Portugal";
-    url = "http://27793.live.streamtheworld.com/RFMAAC.aac";
+    thumbnail = "https://static.mytuner.mobi/media/tvos_radios/2hbumqwx4jdc.png";
   }
   {
     name = "UCB 2";
     url = "https://listen-ucb.sharp-stream.com/55_ucb_2_48_aac";
+    thumbnail = "https://ucb.co.uk/media/pxbflqo0/ucb2.png";
   }
-  ]
+  {
+    name = "RFM Portugal";
+    url = "http://27793.live.streamtheworld.com/RFMAAC.aac";
+    thumbnail = "https://cdn-profiles.tunein.com/s24948/images/logog.png";
+  }
+]
--- home/dotfiles/strawberry.nix
+++ home/dotfiles/strawberry.nix
@@ -4,14 +4,32 @@ let
   # Import radio streams list
   radioStreams = import ./strawberry-radio-streams.nix;
 
-  # Generate SQL statements to insert/update streams
-  generateStreamSQL = stream: ''
-    DELETE FROM radio_channels WHERE name = '${stream.name}' AND source = 0;
-    INSERT INTO radio_channels (source, name, url) VALUES (0, '${stream.name}', '${stream.url}');
+  # Generate SQL statements to insert streams into a playlist
+  # Using type=4 for stream URLs
+  generatePlaylistItemSQL = idx: stream:
+    let
+      thumbnail = if stream ? thumbnail then "'${stream.thumbnail}'" else "NULL";
+    in ''
+    INSERT INTO playlist_items (playlist, type, url, title, art_manual)
+    VALUES (
+      (SELECT ROWID FROM playlists WHERE name = 'Radio Streams' LIMIT 1),
+      4,
+      '${stream.url}',
+      '${stream.name}',
+      ${thumbnail}
+    );
   '';
 
   # Combine all SQL statements
-  sqlStatements = lib.concatMapStrings generateStreamSQL radioStreams;
+  sqlStatements = ''
+    -- Create or update Radio Streams playlist
+    INSERT OR IGNORE INTO playlists (name, ui_order, is_favorite) VALUES ('Radio Streams', 0, 1);
+
+    -- Clear existing items from Radio Streams playlist
+    DELETE FROM playlist_items WHERE playlist = (SELECT ROWID FROM playlists WHERE name = 'Radio Streams' LIMIT 1);
+
+    -- Insert radio streams
+  '' + lib.concatImapStrings generatePlaylistItemSQL radioStreams;
 
   # Script that updates the Strawberry database
   updateStreamsScript = pkgs.writeShellScript "update-strawberry-streams.sh" ''
@@ -21,26 +39,20 @@ let
     # Create directory if it doesn't exist
     mkdir -p "$DB_DIR"
 
-    # Create database and table if they don't exist
+    # Wait for Strawberry database to exist
     if [ ! -f "$DB_FILE" ]; then
-      echo "Creating Strawberry database..."
-      ${pkgs.sqlite}/bin/sqlite3 "$DB_FILE" <<EOF
-    CREATE TABLE IF NOT EXISTS radio_channels (
-      source INTEGER NOT NULL DEFAULT 0,
-      name TEXT,
-      url TEXT NOT NULL,
-      thumbnail_url TEXT
-    );
-    EOF
+      echo "Strawberry database doesn't exist yet, skipping radio streams setup"
+      echo "Start Strawberry once to create the database, then run home-manager switch again"
+      exit 0
     fi
 
-    # Update radio streams
-    echo "Updating Strawberry radio streams..."
+    # Update radio streams playlist
+    echo "Updating Radio Streams playlist..."
     ${pkgs.sqlite}/bin/sqlite3 "$DB_FILE" <<EOF
     ${sqlStatements}
     EOF
 
-    echo "Strawberry radio streams updated successfully"
+    echo "Radio Streams playlist updated successfully"
   '';
 
 in {
